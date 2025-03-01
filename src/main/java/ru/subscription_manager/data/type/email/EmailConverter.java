1e1f5c418037230ee21c647f22329b1a7855c9b1
GG_Host <gd.host@yandex.ru>
2025-03-01 13:42:37 +0300

    feat: email converter updated to user Email type instead of String

--- src/main/java/ru/subscription_manager/data/type/email/EmailConverter.java
+++ src/main/java/ru/subscription_manager/data/type/email/EmailConverter.java
@@ -6,25 +6,25 @@ import jakarta.persistence.Converter;
 import java.util.regex.Pattern;
 
 @Converter
-public class EmailConverter implements AttributeConverter<String, String> {
+public class EmailConverter implements AttributeConverter<Email, String> {
 
     // Регулярное выражение из PostgreSQL-домена
     private static final Pattern EMAIL_PATTERN =
             Pattern.compile("^[\\w\\-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
 
     @Override
-    public String convertToDatabaseColumn(String attribute) {
+    public String convertToDatabaseColumn(Email attribute) {
         if (attribute == null) return null;
 
         // Проверка формата перед сохранением в БД
-        if (!EMAIL_PATTERN.matcher(attribute).matches()) {
+        if (!EMAIL_PATTERN.matcher(attribute.value()).matches()) {
             throw new IllegalArgumentException("Invalid email format");
         }
-        return attribute;
+        return attribute.value();
     }
 
     @Override
-    public String convertToEntityAttribute(String dbData) {
-        return dbData;
+    public Email convertToEntityAttribute(String dbData) {
+        return new Email(dbData);
     }
 }
